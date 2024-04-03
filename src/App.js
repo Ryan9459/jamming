fbeb7eb9b9cbe6739bd8de626734a8a49c68ea71
Ryan9459 <139267006+Ryan9459@users.noreply.github.com>
2024-04-03 12:35:42 +0100

    Organisation
    Organising file structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Jamming</h1>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
